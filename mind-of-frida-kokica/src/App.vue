caa5f214471824102775c214835c5b5dabd4b820
manjana61 <nemanjarisingstars@gmail.com>
2022-09-03 13:27:33 +0200

    Napravio kartice i tablu za igru.

--- mind-of-frida-kokica/src/App.vue
+++ mind-of-frida-kokica/src/App.vue
@@ -1,23 +1,44 @@
 <template>
   <header>
     <h1>Mind of Frida Kokica</h1>
-    <!-- <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
+    <div class="game-board">
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+    </div>
   </header>
-
-  <!-- <RouterView /> -->
 </template>
 
-<script setup>
-/* import { RouterLink, RouterView } from "vue-router"; */
-</script>
+<script setup></script>
 
 <style scoped>
 h1 {
   text-align: center;
+}
+
+.card {
+  border: 5px solid black;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-row-gap: 30px;
+  justify-content: center;
 }
 </style>
