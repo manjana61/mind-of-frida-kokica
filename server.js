79032dd2e679c6d8c13079dcb236520b4c198f57
manjana61 <nemanjarisingstars@gmail.com>
2022-10-10 10:38:54 +0200

    Demo deployment phase.

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ let path = require('path')
 
 let app = express()
 
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 let port = process.env.PORT || 8080
 
 app.listen(port)
 
+console.log(`app is listening on port: ${port}`)
 /* OVO SAM DODAO I U PACKAGE JSON-u U SCRIPTS OBRISATI "start" liniju */
